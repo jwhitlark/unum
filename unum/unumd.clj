fc69bcea6e9927eae93b44632f0bff77ca5fdc44
Jason Whitlark <jw@utopia.(none)>
2009-08-05 19:18:48 -0700

    misc

--- unum/unumd.clj
+++ unum/unumd.clj
@@ -1,15 +1,13 @@
 (ns org.unum
   ;(:use clojure.contrib.str-utils)
-  (:import (org.apache.zookeeper ZooKeeper Watcher CreateMode ZooDefs$Ids))
-  (:import (org.apache.zookeeper.data Stat ACL))
-  (:import (java.net InetAddress))
+  (:use zookeeper)
+  (:use clojure.set)
+;  (:import (org.apache.zookeeper ZooKeeper Watcher CreateMode ZooDefs$Ids))
+;  (:import (org.apache.zookeeper.data Stat ACL))
+;  (:import (java.net InetAddress))
   )
 
-(def hostname (.getHostName (InetAddress/getLocalHost)))
-(def zk (ZooKeeper. "10.17.74.1:2181" 3000 nil))
-;(. ZooDefs$Ids OPEN_ACL_UNSAFE)
-;(def ZD (ZooDefs/Ids.))
-;(.Ids temp_ids)
+(connect)
 
 ;Register the hostname in static
 ;Register the hostname in current
@@ -17,33 +15,73 @@
 (defn sleep [secs]
   (Thread/sleep (* secs 1000)))
 
-(defn getUnumData [path]
-  (String. (.getData zk path false (Stat.))))
+; This is duplicated with the register stuff lower, find a way to collapse it.
+(defn create-named-znode [pth]
+  (fn []
+    (let [path pth]
+      (when-not (znode-exists? path)
+	(create-perm-znode path "")))))
 
-(defn createPermZNode [path data]
-  (.create zk path (.getBytes data) ZooDefs$Ids/OPEN_ACL_UNSAFE CreateMode/PERSISTENT))
+(def create-account-znode
+     (create-named-znode "/unum/accounts"))
 
-(defn createTempZNode [path data]
-  (.create zk path (.getBytes data) ZooDefs$Ids/OPEN_ACL_UNSAFE CreateMode/EPHEMERAL))
-
-; try java.net.NetworkInterface  getNetworkInterfaces
+(def create-service-znode
+     (create-named-znode "/unum/services"))
 
 
-; (CreateMode/EPHEMERAL)
+(defn initial-setup []
+  (do
+    (create-account-znode)
+    (create-service-znode)))
 
-(println "Greetings from" hostname)
+(defn register []
+  (let [path (str "/unum/static/" hostname)
+	now (str (System/currentTimeMillis))] ;in milliseconds!
+    (when-not (znode-exists? path)
+      (create-perm-znode path now))))
 
-(println (getUnumData "/unum"))
+(defn logon []
+  (let [path (str "/unum/current/" hostname)
+	now (str (System/currentTimeMillis))] ;in milliseconds!
+    (create-temp-znode path now)))
 
-(.setData zk "/unum" (.getBytes "It worked!") -1)
 
-(println (getUnumData "/unum"))
+(defn unum-test []
+  (println "Greetings from" hostname)
 
-;(println (.exists zk "/unum" false))
-(println (.exists zk "/foobar" false))
+  (println (format "Verifying /baz does not exist: %s" (znode-exists? "/baz")))
+  (println (format "Verifying /unum exists: %s" (znode-exists? "/unum")))
 
-;(.create zk "/foobar" (.getBytes "new data") ZooDefs$Ids/OPEN_ACL_UNSAFE CreateMode/EPHEMERAL)
-(createTempZNode "/foobar" "my test")
-(sleep 5)
+  (set-znode-data "/unum" "initial set")
+  (println (get-znode-data "/unum"))
 
-(println (.exists zk "/foobar" false))
+  (set-znode-data "/unum" "change ok")
+  (println (get-znode-data "/unum"))
+
+  (create-temp-znode "/mytest" "a")
+  (println "setting watch on /mytest")
+  (watch-znode "/mytest" (watcher))
+  (set-znode-data "/mytest" "b")
+
+
+  ;Infinite loop
+  (loop [] (recur))
+)
+
+
+;(unum-test)
+(initial-setup)
+(register)
+(logon)
+(let [perm (get-znode-children "/unum/static")
+      cur (get-znode-children "/unum/current")]
+  (do
+    (println perm)
+    (println cur)
+    (println (difference perm cur))))
+
+(loop [] (recur))
+
+;TODO: embed intrepreter
+;see http://clojure101.blogspot.com/2009/05/creating-clojure-repl-in-your.html
+;(def server-socket (org.enclojure.repl.main/run-repl-server 11345))
