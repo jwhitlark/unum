37440dbfcdca56927415889e882462e35e80cd84
Jason Whitark <jason@whitlark.org>
2011-04-27 18:02:51 -0700

    Bumped versions to latest.

--- project.clj
+++ project.clj
@@ -5,11 +5,14 @@
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [org.apache.activemq/activemq-core "5.4.0"]
-		 [org.apache.activemq/activemq-camel "5.4.0"]
+		 [org.apache.activemq/activemq-core "5.5.0"]
+		 [org.apache.activemq/activemq-camel "5.5.0"]
 		 ;;		 [activemq/activemq-transport-xstream "2.1"]
 		 [com.thoughtworks.xstream/xstream "1.3.1"]
-		 [org.apache.camel/camel-core "2.4.0"]
+		 [org.apache.camel/camel-core "2.7.0"]
+		 ;; Perhaps use clutch instead?
 		 [clojure-couchdb "0.4.5"]
-		 [swank-clojure "1.2.1"]
+		 [org.slf4j/slf4j-api "1.6.1"]
+		 [org.slf4j/slf4j-simple "1.6.1"]
+		 [swank-clojure "1.4.0-SNAPSHOT"]
 		 ])
